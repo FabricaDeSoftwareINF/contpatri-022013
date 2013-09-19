9ff260b0ae3a8cbc9f0441da8ef297e66edc1318
halissonvit <halissonvit@gmail.com>
2013-09-19 22:36:08 +0000

    Alterando módulo de dominio para que seja possível adicionar um objeto do tipo subLocal ao Tombamento, e fazendo bump da versão

--- Desenvolvimento/contpatri-model/src/main/java/br/ufg/inf/es/fs/contpatri/model/Tombamento.java
+++ Desenvolvimento/contpatri-model/src/main/java/br/ufg/inf/es/fs/contpatri/model/Tombamento.java
@@ -24,190 +24,196 @@ import java.util.Date;
 
 /**
  * Código de tombamento de um bem patrimonial Imutável.
- * 
+ *
  * @author Emerson Jose Porfirio
  */
 public class Tombamento implements Serializable, Cloneable {
 
-	/**
-	 * UID
-	 */
-	private static final long serialVersionUID = 2967724032422394187L;
-	
-	/**
-	 * Id do tombamento
-	 */
-	private Long id;
-	
-	/**
-	 * Codigo do tombamento
-	 */
-	private String codTombamento;
-	
-	/**
-	 * Data do tombamento
-	 */
-	private Date dataTombamento;
-	
-	/**
-	 * Tipo do tombamento
-	 */
-	private TipoTombamento tipoTombamento;
+    /**
+     * UID
+     */
+    private static final long serialVersionUID = 2967724032422394187L;
+    /**
+     * Id do tombamento
+     */
+    private Long id;
+    /**
+     * Codigo do tombamento
+     */
+    private String codTombamento;
+    /**
+     * Data do tombamento
+     */
+    private Date dataTombamento;
+    /**
+     * Tipo do tombamento
+     */
+    private TipoTombamento tipoTombamento;
+    /**
+     * SubLocal do tombamento
+     */
+    private SubLocal subLocal;
 
-	/**
-	 * Obtem o tipo do tombamento
-	 * 
-	 * @return Tipo do tombamento
-	 */
-	public TipoTombamento getTipoTombamento() {
-		return tipoTombamento;
-	}
+    /**
+     * Construtor da classe
+     */
+    public Tombamento() {
+    }
 
-	/**
-	 * Define o tipo do tombamento
-	 * 
-	 * @param tipoTombamento Tipo do tombamento
-	 */
-	public final void setTipoTombamento(TipoTombamento tipoTombamento) {
-		this.tipoTombamento = tipoTombamento;
-	}
+    /**
+     * Construtor de um tombamento
+     *
+     * @param codTombamento  Codigo do tombamento
+     * @param dataTombamento Data do tombamento
+     * @param tipoTombamento Tipo do tombamento
+     */
+    public Tombamento(String codTombamento, Date dataTombamento,
+                      TipoTombamento tipoTombamento) {
+        setCodTombamento(codTombamento);
+        setDataTombamento(dataTombamento);
+        setTipoTombamento(tipoTombamento);
+    }
 
-	/**
-	 * Construtor da classe
-	 */
-	public Tombamento() {
-	}
+    public SubLocal getSubLocal() {
+        return subLocal;
+    }
 
-	/**
-	 * Construtor de um tombamento
-	 * 
-	 * @param codTombamento Codigo do tombamento
-	 * 
-	 * @param dataTombamento Data do tombamento
-	 * 
-	 * @param tipoTombamento Tipo do tombamento
-	 */
-	public Tombamento(String codTombamento, Date dataTombamento,
-			TipoTombamento tipoTombamento) {
-		setCodTombamento(codTombamento);
-		setDataTombamento(dataTombamento);
-		setTipoTombamento(tipoTombamento);
-	}
+    public void setSubLocal(SubLocal subLocal) {
+        this.subLocal = subLocal;
+    }
 
-	/**
-	 * Obtem a id do tombamento
-	 * 
-	 * @return Id do tombamento
-	 */
-	public Long getId() {
-		return id;
-	}
+    /**
+     * Obtem o tipo do tombamento
+     *
+     * @return Tipo do tombamento
+     */
+    public TipoTombamento getTipoTombamento() {
+        return tipoTombamento;
+    }
 
-	/**
-	 * Define o id do tombamento
-	 * 
-	 * @param id Id do tombamento
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
+    /**
+     * Define o tipo do tombamento
+     *
+     * @param tipoTombamento Tipo do tombamento
+     */
+    public final void setTipoTombamento(TipoTombamento tipoTombamento) {
+        this.tipoTombamento = tipoTombamento;
+    }
 
-	/**
-	 * Número que identifica o tombamento.Esse código fica afixado ao bem seja
-	 * por uma plaqueta metálica ou por uma etiqueta comum (código de barras,
-	 * QR-code ou somente números).
-	 * 
-	 * @return String
-	 */
-	public String getCodTombamento() {
-		return codTombamento;
-	}
+    /**
+     * Obtem a id do tombamento
+     *
+     * @return Id do tombamento
+     */
+    public Long getId() {
+        return id;
+    }
 
-	/**
-	 * Define o codigo do tombamento
-	 * 
-	 * @param codTombamento Codigo do tombamento
-	 */
-	private void setCodTombamento(String codTombamento) {
-		if (codTombamento == null || codTombamento.trim().isEmpty()) {
-			throw new IllegalArgumentException(
-					"O código de tombamento deve ser informado");
-		}
-		this.codTombamento = codTombamento;
-	}
+    /**
+     * Define o id do tombamento
+     *
+     * @param id Id do tombamento
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	/**
-	 * Data em que o tombamento foi realizado
-	 * 
-	 * @return String
-	 */
-	public Date getDataTombamento() {
-		return dataTombamento;
-	}
+    /**
+     * Número que identifica o tombamento.Esse código fica afixado ao bem seja
+     * por uma plaqueta metálica ou por uma etiqueta comum (código de barras,
+     * QR-code ou somente números).
+     *
+     * @return String
+     */
+    public String getCodTombamento() {
+        return codTombamento;
+    }
 
-	/**
-	 * Define a data do tombamento
-	 * 
-	 * @param dataTombamento Data do tombamento
-	 */
-	private void setDataTombamento(Date dataTombamento) {
-		if (dataTombamento == null) {
-			throw new IllegalArgumentException(
-					"A data de tombamento deve ser informada");
-		}
-		this.dataTombamento = dataTombamento;
-	}
+    /**
+     * Define o codigo do tombamento
+     *
+     * @param codTombamento Codigo do tombamento
+     */
+    private void setCodTombamento(String codTombamento) {
+        if (codTombamento == null || codTombamento.trim().isEmpty()) {
+            throw new IllegalArgumentException(
+                    "O código de tombamento deve ser informado");
+        }
+        this.codTombamento = codTombamento;
+    }
 
-	/**
-	 * Clona um tombamento
-	 */
-	@Override
-	public Tombamento clone() throws CloneNotSupportedException {
-		Tombamento clone = (Tombamento) super.clone();
-		clone.setDataTombamento((Date) this.dataTombamento.clone());
-		return clone;
-	}
+    /**
+     * Data em que o tombamento foi realizado
+     *
+     * @return String
+     */
+    public Date getDataTombamento() {
+        return dataTombamento;
+    }
 
-	/**
-	 * Compara dois tombamento
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (obj == this) {
-			return true;
-		}
-		if (!(obj instanceof Tombamento)) {
-			return false;
-		}
-		Tombamento outro = (Tombamento) obj;
-		if (this.dataTombamento.equals(outro.dataTombamento)) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Define a data do tombamento
+     *
+     * @param dataTombamento Data do tombamento
+     */
+    private void setDataTombamento(Date dataTombamento) {
+        if (dataTombamento == null) {
+            throw new IllegalArgumentException(
+                    "A data de tombamento deve ser informada");
+        }
+        this.dataTombamento = dataTombamento;
+    }
 
-	/**
-	 * HashCode do tombamento
-	 */
-	@Override
-	public int hashCode() {
-		final int valorInicial = 7;
-		final int valorIncremental = 89;
+    /**
+     * Clona um tombamento
+     */
+    @Override
+    public Tombamento clone() throws CloneNotSupportedException {
+        Tombamento clone = (Tombamento) super.clone();
+        clone.setDataTombamento((Date) this.dataTombamento.clone());
+        return clone;
+    }
 
-		int hash = valorInicial;
-		hash = valorIncremental * hash
-				+ (this.id != null ? this.id.hashCode() : 0);
-		hash = valorIncremental
-				* hash
-				+ (this.codTombamento != null ? this.codTombamento.hashCode()
-						: 0);
-		hash = valorIncremental
-				* hash
-				+ (this.dataTombamento != null ? this.dataTombamento.hashCode()
-						: 0);
-		return hash;
-	}
+    /**
+     * Compara dois tombamento
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Tombamento)) {
+            return false;
+        }
+        Tombamento outro = (Tombamento) obj;
+        if (this.dataTombamento.equals(outro.dataTombamento)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * HashCode do tombamento
+     */
+    @Override
+    public int hashCode() {
+        final int valorInicial = 7;
+        final int valorIncremental = 89;
+
+        int hash = valorInicial;
+        hash = valorIncremental * hash
+                + (this.id != null ? this.id.hashCode() : 0);
+        hash = valorIncremental
+                * hash
+                + (this.codTombamento != null ? this.codTombamento.hashCode()
+                : 0);
+        hash = valorIncremental
+                * hash
+                + (this.dataTombamento != null ? this.dataTombamento.hashCode()
+                : 0);
+        return hash;
+    }
 }
