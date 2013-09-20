02c9735cef88f684a3366055b473c1595ad03634
rogerio.tju@gmail.com <rogerio.tju@gmail.com@a05893f0-2596-1e2e-a400-38bb3da4bf57>
2013-09-20 18:12:23 +0000

    Retornando os testes do módulo de persistência.

---
+++ trunk/Desenvolvimento/contpatri-persistencia/src/test/java/br/ufg/inf/es/fs/contpatri/persistencia/HibernateUtilTest.java
@@ -0,0 +1,68 @@
+/*
+ * Esse documento é parte do código fonte e artefatos relacionados ao projeto
+ * CONTPATRI, em desenvolvimento pela Fábrica de Software da UFG.
+ * 
+ * Links relevantes: Fábrica de Software: http://fs.inf.ufg.br/ Instituto de
+ * Informática UFG: http://www.inf.ufg.br Projeto CONTPATRI DROPBOX:
+ * https://www.dropbox.com/home/CONTPATRI%20-%20012013 Projeto CONTPATRI
+ * REDMINE:
+ * 
+ * Copyleft © UFG.
+ * 
+ * Licenciado sobre a licença GNU-GPL v3
+ * 
+ * Você pode obter uma cópia da licença em http://www.gnu.org/licenses/gpl.html
+ * 
+ * A menos que especificado ou exigido por legislação local, o software é
+ * fornecido "da maneira que está", sem garantias ou condições de qualquer tipo,
+ * nem expressas nem implícitas. Em caso de dúvidas referir a licença GNU-GPL.
+ */
+package br.ufg.inf.es.fs.contpatri.persistencia;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+
+/**
+ * 
+ * Teste do HibernateUtil
+ *
+ */
+public class HibernateUtilTest {
+
+	@Test
+	/**
+	 * Teste
+	 */
+	public void testHibernateUtils() {
+		HibernateUtil util = new HibernateUtil();
+		
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = HibernateUtil.getSession();
+		
+		try{
+			HibernateUtil.beginTransaction();
+			HibernateUtil.commitTransaction();
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+		} finally {
+			HibernateUtil.closeSession();
+		}
+		
+		try{
+			util.toString();
+			HibernateUtil.beginTransaction();
+			HibernateUtil.rollbackTransaction();
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+		} finally {
+			HibernateUtil.closeSession();
+		}
+		
+		assertNotNull(sessionFactory);
+		assertNotNull(session);
+	}
+
+}
